37125b63bff9d3f0d2691309c609a1f8ca34f831
Nikola Whallon <browncanstudios@gmail.com>
2022-08-15 13:36:03 -0700

    decoupled dungeon generation from tile population

--- Scenes/Dungeon.gd
+++ Scenes/Dungeon.gd
@@ -8,15 +8,20 @@ export(int)   var ground_condition = 3
 
 enum Tiles { GROUND, WALL }
 
-# TODO: I don't love the logic for getting the biggest cave, although it works
-# once this is re-worked to decouple cave creation from tile populating,
-# maybe the logic can be refined to my liking
+var tiles = []
+
 var biggest_cave
 
 var rng = RandomNumberGenerator.new()
 
 func _ready():
 	rng.randomize()
+	
+	for x in range(0, map_w):
+		var column = []
+		column.resize(map_h)
+		tiles.append(column)
+
 	generate()
 
 func generate():
@@ -26,47 +31,59 @@ func generate():
 	double_vertically()
 	dig_caves()
 	get_biggest_cave()
-	
-# start by filling the map with wall tiles
+	remove_vertical_singles()
+	convert_tile_array_to_tilemap()
+
+func convert_tile_array_to_tilemap():
+	for x in range(0, map_w):
+		for y in range(0, map_h):
+			set_cell(x, y, tiles[x][y])
+
 func fill_wall():
 	for x in range(0, map_w):
 		for y in range(0, map_h):
-			set_cell(x, y, Tiles.WALL)
+			tiles[x][y] = Tiles.WALL
 
-# then randomly place ground tiles
 func random_ground():
 	for x in range(1, map_w - 1):
 		for y in range(1, map_h - 1):
 			if rng.randf() < ground_seed:
-				set_cell(x, y, Tiles.GROUND)
+				tiles[x][y] = Tiles.GROUND
 
 func double_vertically():
 	for x in range(1, map_w - 1):
 		for y in range(1, map_h - 1):
-			if get_cell(x, y) == Tiles.WALL:
-				if get_cell(x, y - 1) != Tiles.WALL and get_cell(x, y + 1) != Tiles.WALL:
-					set_cell(x, y + 1, Tiles.WALL)
+			if tiles[x][y] == Tiles.WALL:
+				if tiles[x][y - 1] != Tiles.WALL and tiles[x][y + 1] != Tiles.WALL:
+					tiles[x][y + 1] = Tiles.WALL
+
+func remove_vertical_singles():
+	for x in range(1, map_w - 1):
+		for y in range(1, map_h - 1):
+			if tiles[x][y] == Tiles.WALL:
+				if tiles[x][y - 1] != Tiles.WALL and tiles[x][y + 1] != Tiles.WALL:
+					tiles[x][y] = Tiles.GROUND
 
 func dig_caves():
 	for _i in range(10):
 		for x in range(1, map_w - 1):
 			for y in range(1, map_h - 1):
 				if check_nearby_walls(x, y) > wall_condition:
-					set_cell(x, y, Tiles.WALL)
+					tiles[x][y] = Tiles.WALL
 				elif check_nearby_walls(x, y) < ground_condition:
-					set_cell(x, y, Tiles.GROUND)
+					tiles[x][y] = Tiles.GROUND
 
 # check in 8 dirs to see how many tiles are walls
 func check_nearby_walls(x, y):
 	var count = 0
-	if get_cell(x, y - 1)    == Tiles.WALL:  count += 1
-	if get_cell(x, y + 1)    == Tiles.WALL:  count += 1
-	if get_cell(x - 1, y)    == Tiles.WALL:  count += 1
-	if get_cell(x + 1, y)    == Tiles.WALL:  count += 1
-	if get_cell(x + 1, y + 1) == Tiles.WALL:  count += 1
-	if get_cell(x + 1, y - 1) == Tiles.WALL:  count += 1
-	if get_cell(x - 1, y + 1) == Tiles.WALL:  count += 1
-	if get_cell(x - 1, y - 1) == Tiles.WALL:  count += 1
+	if tiles[x][y - 1]    == Tiles.WALL:  count += 1
+	if tiles[x][y + 1]    == Tiles.WALL:  count += 1
+	if tiles[x - 1][y]    == Tiles.WALL:  count += 1
+	if tiles[x + 1][y]    == Tiles.WALL:  count += 1
+	if tiles[x + 1][y + 1] == Tiles.WALL:  count += 1
+	if tiles[x + 1][y - 1] == Tiles.WALL:  count += 1
+	if tiles[x - 1][y + 1] == Tiles.WALL:  count += 1
+	if tiles[x - 1][y - 1] == Tiles.WALL:  count += 1
 	return count
 
 func get_biggest_cave():
@@ -74,19 +91,12 @@ func get_biggest_cave():
 	
 	for x in range (0, map_w):
 		for y in range (0, map_h):
-			if get_cell(x, y) == Tiles.GROUND:
+			if tiles[x][y] == Tiles.GROUND:
 				flood_fill(x, y)
 
 	for tile in biggest_cave:
-		set_cellv(tile, Tiles.GROUND)
+		tiles[tile.x][tile.y] = Tiles.GROUND
 
-# this function takes a GROUND tile and does a flood fill
-# making it and neighboring tiles WALL tiles, but the cells
-# are kept track of in the biggest_cave variable if the cave
-# created by the flood fill is biggest cave
-# then, the biggest cave can be filled back with GROUND
-# TODO: this flood fill ought not change tiles, but just return the
-# arrays of tiles which define a cave
 func flood_fill(x, y):
 	var cave = []
 	var to_fill = [Vector2(x, y)]
@@ -95,7 +105,7 @@ func flood_fill(x, y):
 
 		if !cave.has(tile):
 			cave.append(tile)
-			set_cellv(tile, Tiles.WALL)
+			tiles[tile.x][tile.y] = Tiles.WALL
 
 			# check adjacent cells
 			var north = Vector2(tile.x, tile.y - 1)
@@ -104,7 +114,7 @@ func flood_fill(x, y):
 			var west  = Vector2(tile.x - 1, tile.y)
 
 			for neighbor in [north, south, east, west]:
-				if get_cellv(neighbor) == Tiles.GROUND:
+				if tiles[neighbor.x][neighbor.y] == Tiles.GROUND:
 					if !to_fill.has(neighbor) and !cave.has(neighbor):
 						to_fill.append(neighbor)
 
@@ -112,7 +122,7 @@ func flood_fill(x, y):
 		biggest_cave = cave
 
 func is_ground(x, y):
-	if get_cell(x, y) == Tiles.GROUND:
+	if tiles[x][y] == Tiles.GROUND:
 		return true
 
 	return false
